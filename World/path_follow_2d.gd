e6410142b3807375ee0fe7ddf173ebff4a4fd278
Storky444 <storkanovaa@gmail.com>
2026-02-26 12:13:20 +0100

    Přidání malého Scorpiona

--- World/path_follow_2d.gd
+++ World/path_follow_2d.gd
@@ -1,7 +1,25 @@
 extends PathFollow2D
+
+
+
+var direction := 1
 var speed : float
+
 func _ready():
+	z_index = 5
 	speed = randf_range(100,200)
-	
+
 func _process(delta):
-	progress += speed * delta
+	progress += speed * direction * delta
+
+	if progress_ratio >= 1.0:
+		direction = -1
+		flip()
+
+	if progress_ratio <= 0.0:
+		direction = 1
+		flip()
+
+func flip():
+	$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
+	
