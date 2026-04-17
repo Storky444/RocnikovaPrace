61ecdc7e9d62c423d65d7d29e4b026b0eab1115b
Storky444 <storkanovaa@gmail.com>
2026-04-17 23:56:30 +0200

    Sprovoznění 2 buttonů v menu

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://World/world.tscn")
+
+
+func _on_tutorial_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
